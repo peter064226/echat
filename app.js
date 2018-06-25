7d087e1bbd8e5b68ccaa5c0bc6092e315456bac4
william <william807803@hotmail.com>
2018-06-25 20:25:34 +0800

    创建目录

---
+++ app.js
@@ -0,0 +1,11 @@
+let express = require('express');
+let app = express();
+app.get('/',function(req,res){
+	res.send('Hello world');
+})
+app.set('view engine','html');
+app.engine('.html',require('ejs').__express);
+app.set('views',require('path'),join(__dirname,'views');
+app.use(express.static(require('path').join(__dirname, 'public')));
+
+app.listen(80);
