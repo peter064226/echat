8b71d9d9b55a3634526fc20bcb796399cd126422
Unknown <william807803@hotmail.com>
2018-06-25 22:49:34 +0800

    添加登陆页面

--- app.js
+++ app.js
@@ -51,6 +51,30 @@ app.use(function(req, res, next){
     if (err) res.locals.message = '<div class="alert alert-danger" style="margin-bottom: 20px;color:red;">' + err + '</div>';
     next();
 });
+app.get('/login',function(req,res){
+    res.render('login');
+});
+app.post('/login', function (req, res) {
+        var User = global.dbHelper.getModel('user'),
+            uname = req.body.uname;
+        User.findOne({name: uname}, function (error, doc) {
+            if (error) {
+                res.send(500);
+                console.log(error);
+            } else if (!doc) {
+                req.session.error = '用户名不存在！';
+                res.send(404);
+            } else {
+               if(req.body.upwd != doc.password){
+                   req.session.error = "密码错误!";
+                   res.send(404);
+               }else{
+                   req.session.user=doc;
+                   res.send(200);
+               }
+            }
+        });
+    });
 app.set('view engine','html');
 app.engine('.html',require('ejs').__express);
 app.set('views', require('path').join(__dirname, 'views'));	
